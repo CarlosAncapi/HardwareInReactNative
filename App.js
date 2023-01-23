cbc6ca9a73db55115beb215d596f323545f5b081
CarlosAncapi <115656721+CarlosAncapi@users.noreply.github.com>
2023-01-23 10:18:23 -0300

    marcadores en mapa

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
-import { React, useEffect } from 'react';
-import { Alert, StyleSheet, Text, View } from 'react-native';
+import  React,{ useState,  useEffect } from 'react';
+import { Alert, Dimensions, StyleSheet, Text, View } from 'react-native';
+import MapView, {Marker} from 'react-native-maps';
 import * as Location from 'expo-location';
 import Constants from 'expo-constants';
 
 export default function App() {
+  const [locacion, setLocacion] = useState({})
   const buscaLocation = async () =>{
-    const { status } = await Location.getForegroundPermissionsAsync()
+    const { status } = await Location.requestBackgroundPermissionsAsync()
     if ( status !== 'granted') {
       return Alert.alert('no tenemos los permisos necesarios para acceder a la localizacion')
     }
@@ -18,12 +20,26 @@ export default function App() {
 
   return (
     <View style={styles.container}>
+      <MapView style={styles.map}> 
+        {locacion.coords
+          ? <Marker
+              coordinate={locacion.coords}
+              title="Titulo"
+              deecription="descripccion del punto" />
+            :null
+        }
+
+      </MapView>
       
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
